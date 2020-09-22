1a246c8c93b9a6ec1cd216049214e28ab90eb7f3
SourFruit   <65507526+hack-everything@users.noreply.github.com>
2020-09-22 19:12:10 -0400

    Add files via upload

---
+++ pi.js
@@ -0,0 +1,5 @@
+console.log('hey..... pssssst')
+console.log('Email us the code: CONSOLE3')
+console.log('To get 1 month of Pi Zero hosting for $3!')
+console.log('That\'s $1 per Pi, for a month!')
+
